e6ee1bbdbf3cbe7fbc3c5f511132168c2ec4d9c0
Yaël Dillies <yael.dillies@gmail.com>
2024-07-25 21:09:58 +0000

    Switch to Patrick's new blueprint infrastructure

--- lakefile.lean
+++ lakefile.lean
@@ -15,5 +15,7 @@ meta if get_config? env = some "dev" then -- dev is so not everyone has to build
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
 
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
 @[default_target]
 lean_lib ChandraFurstLipton
