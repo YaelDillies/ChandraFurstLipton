f2e5b051e71b227db08133d3e3385b8e9f9447f2
Isabel Dahlgren <isabel.dahlgren@gmail.com>
2024-07-26 16:53:27 +0300

    Add length of broadcast

--- ChandraFurstLipton/NOFModel.lean
+++ ChandraFurstLipton/NOFModel.lean
@@ -13,11 +13,24 @@ structure Strategy where
 
 namespace Strategy
 
-def broadcast (S : Strategy G d) (x : Fin d → G) (i : Fin d) : ℕ → List Bool
+def broadcast (S : Strategy G d) (x : Fin d → G) : ℕ → List Bool
   | 0 => []
-  | t + 1 => S.nextBit i (forget i x) (broadcast S x i t) :: (broadcast S x i t)
+  | t + 1 => S.nextBit t (forget t x) (broadcast S x t) :: broadcast S x t
+
+lemma broadcast_zero (S : Strategy G d) (x : Fin d → G) : broadcast S x 0 = [] :=
+  rfl
+
+lemma broadcast_succ (S : Strategy G d) (x : Fin d → G) (t : ℕ) :
+  broadcast S x (t + 1) = S.nextBit t (forget t x) (broadcast S x t) :: broadcast S x t :=
+  rfl
 
 def IsValid (S : Strategy G d) (F : (Fin d → G) → G) (t : ℕ) : Prop :=
-  ∀ x : Fin d → G, ∀ i : Fin d, S.guess i (forget i x) (broadcast S x i t) = F x
+  ∀ x : Fin d → G, ∀ i : Fin d, S.guess i (forget i x) (broadcast S x t) = F x
+
+theorem length_broadcast (S : Strategy G d) (x : Fin d → G) (t : ℕ) : (broadcast S x t).length = t := by
+  induction' t with t ih
+  · simp [broadcast_zero S x]
+  · simp [broadcast_succ S x t]
+    exact ih
 
 end NOF.Strategy
