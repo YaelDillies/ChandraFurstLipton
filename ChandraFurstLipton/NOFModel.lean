e80dd97840098d03537bc291d269cbc42fc1f11a
Yaël Dillies <yael.dillies@gmail.com>
2024-07-28 07:56:53 +0000

    Rename "strategy" to "protocol"

--- ChandraFurstLipton/NOFModel.lean
+++ ChandraFurstLipton/NOFModel.lean
@@ -13,11 +13,11 @@ def forget (i : Fin d) (x : Fin d → G) (j : {j : Fin d // j ≠ i}) : G :=
   x j
 
 variable (G d) in
-structure Strategy where
+structure Protocol where
   nextBit (i : Fin d) : ({j : Fin d // j ≠ i} → G) → List Bool → Bool
   guess (i : Fin d) : ({j : Fin d // j ≠ i} → G) → List Bool → Bool
 
-namespace Strategy
+namespace Protocol
 
 def getBits (B : List Bool) (i : ℕ) (d : ℕ) : List Bool := Id.run do
   let mut L := []
@@ -26,50 +26,50 @@ def getBits (B : List Bool) (i : ℕ) (d : ℕ) : List Bool := Id.run do
   pure L
 
 noncomputable
-def trivial (hd : 3 ≤ d) (F : (Fin d → G) → Bool) : Strategy G d where
+def trivial (hd : 3 ≤ d) (F : (Fin d → G) → Bool) : Protocol G d where
   nextBit i x B := by
     refine (Nat.bits (Fintype.equivFin G (x ⟨i + 1, ?_ ⟩))).getI (B.length / d)
     rw [Ne, add_right_eq_self, ← Nat.cast_one, Fin.natCast_eq_zero, Nat.dvd_one]
     omega
   guess i x B := F fun j => if h : j = i then (Fintype.equivFin G).invFun (BitVec.toNat (BitVec.ofBoolListLE (getBits B i d))) else x ⟨j, h⟩
 
-def broadcast (S : Strategy G d) (x : Fin d → G) : ℕ → List Bool
+def broadcast (S : Protocol G d) (x : Fin d → G) : ℕ → List Bool
   | 0 => []
   | t + 1 => S.nextBit t (forget t x) (broadcast S x t) :: broadcast S x t
 
 @[simp]
-lemma broadcast_zero (S : Strategy G d) (x : Fin d → G) : broadcast S x 0 = [] :=
+lemma broadcast_zero (S : Protocol G d) (x : Fin d → G) : broadcast S x 0 = [] :=
   rfl
 
-lemma broadcast_succ (S : Strategy G d) (x : Fin d → G) (t : ℕ) :
+lemma broadcast_succ (S : Protocol G d) (x : Fin d → G) (t : ℕ) :
   broadcast S x (t + 1) = S.nextBit t (forget t x) (broadcast S x t) :: broadcast S x t :=
   rfl
 
 @[simp]
-def IsValid (S : Strategy G d) (F : (Fin d → G) → Bool) (t : ℕ) : Prop :=
+def IsValid (S : Protocol G d) (F : (Fin d → G) → Bool) (t : ℕ) : Prop :=
   ∀ x : Fin d → G, ∀ i : Fin d, S.guess i (forget i x) (broadcast S x t) = F x
 
 @[simp]
-theorem length_broadcast (S : Strategy G d) (x : Fin d → G) : ∀ t, (broadcast S x t).length = t
+theorem length_broadcast (S : Protocol G d) (x : Fin d → G) : ∀ t, (broadcast S x t).length = t
   | 0 => rfl
   | t + 1 => by simpa [broadcast_succ] using length_broadcast _ _ _
 
 noncomputable
-def complexity (S : Strategy G d) (F : (Fin d → G) → Bool) : ENat :=
+def complexity (S : Protocol G d) (F : (Fin d → G) → Bool) : ENat :=
   ⨅ (t : ℕ) (_ : IsValid S F t), t
 
-lemma le_complexity {t : ℕ} {F : (Fin d → G) → Bool} {S : Strategy G d} :
-    t ≤ Strategy.complexity S F ↔ ∀ r : ℕ, S.IsValid F r → t ≤ r := by
-  simp [Strategy.complexity]
+lemma le_complexity {t : ℕ} {F : (Fin d → G) → Bool} {S : Protocol G d} :
+    t ≤ Protocol.complexity S F ↔ ∀ r : ℕ, S.IsValid F r → t ≤ r := by
+  simp [Protocol.complexity]
 
-end Strategy
+end Protocol
 
 noncomputable
 def funComplexity (F : (Fin d → G) → Bool) :=
-  ⨅ (S : Strategy G d), S.complexity F
+  ⨅ (S : Protocol G d), S.complexity F
 
 lemma le_funComplexity {t : ℕ} {F : (Fin d → G) → Bool} :
-    t ≤ funComplexity F ↔ ∀ S : Strategy G d, t ≤ S.complexity F := by
+    t ≤ funComplexity F ↔ ∀ S : Protocol G d, t ≤ S.complexity F := by
   simp [funComplexity]
 
 def IsForbiddenPatternWithTip (a : Fin d → Fin d → G) (v : Fin d → G) : Prop :=
@@ -78,15 +78,15 @@ def IsForbiddenPatternWithTip (a : Fin d → Fin d → G) (v : Fin d → G) : Pr
 def IsForbiddenPattern (a : Fin d → Fin d → G) : Prop :=
   ∃ v : Fin d → G, IsForbiddenPatternWithTip a v
 
-lemma IsForbiddenPatternWithTip.broadcast_eq {S : Strategy G d} {t : ℕ} {a : Fin d → Fin d → G}
+lemma IsForbiddenPatternWithTip.broadcast_eq {S : Protocol G d} {t : ℕ} {a : Fin d → Fin d → G}
     {v : Fin d → G} (hF : IsForbiddenPatternWithTip a v) {B : List Bool}
     (hB : ∀ i, S.broadcast (a i) t = B) : S.broadcast v t = B := by
     induction' t with t ih generalizing B
     · simp
       specialize hB 1
-      rw [Strategy.broadcast] at hB
+      rw [Protocol.broadcast] at hB
       exact hB
-    · rw [Strategy.broadcast]
+    · rw [Protocol.broadcast]
       -- Show that the next player in v_{t + 1 mod k} and w see the same thing.
       -- have : forget (t + 1) a (t + 1) = v t := sorry
       sorry
